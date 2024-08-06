4c16a7dc025b78a1668bc448cffc83b9b599b73d
Barrett Edwards <thequakemech@gmail.com>
2024-08-06 06:24:19 -0700

    Reving the python version to 0.2.0 to trigger any importers of the library to download this new version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup
 
 setup(name='nitrile',
-      version='0.1.0',
+      version='0.2.0',
       description='Latex Document Creation Library',
       url='https://github.com/quakemech/nitrile',
       author='Barrett Edwards',
